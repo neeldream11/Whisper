ca5889ab7b3f8bbec4a2dd401570e1a1ba090296
Shwet <icios@dream11.com>
2016-08-09 15:04:37 +0530

    update font and color

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -3,10 +3,10 @@ import UIKit
 public struct ColorList {
 
   public struct Shout {
-    public static var background = UIColor.whiteColor()
+    public static var background = UIColor(red: 222.0 / 255.0, green: 77.0 / 255.0, blue: 59.0 / 255.0, alpha: 1.0)
     public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
-    public static var title = UIColor.blackColor()
-    public static var subtitle = UIColor.blackColor()
+    public static var title = UIColor.whiteColor()
+    public static var subtitle = UIColor.whiteColor()
   }
 
   public struct Whistle {
--- Source/Configuration/FontList.swift
+++ Source/Configuration/FontList.swift
@@ -3,8 +3,8 @@ import UIKit
 public struct FontList {
 
   public struct Shout {
-    public static var title = UIFont.boldSystemFontOfSize(15)
-    public static var subtitle = UIFont.systemFontOfSize(13)
+    public static var title = UIFont(name: "Montserrat-Regular", size: 12.5)
+    public static var subtitle = UIFont(name: "Montserrat-Regular", size: 12.5)
   }
 
   public struct Whistle {
