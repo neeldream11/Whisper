bfc6f7169055936e82a9b82b987201da92c06833
Shwet <shwet@dream11.com>
2016-11-25 16:53:51 +0530

    Update ColorList.swift
    Color updated

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -3,7 +3,7 @@ import UIKit
 public struct ColorList {
 
   public struct Shout {
-    public static var background = UIColor(red: 222.0 / 255.0, green: 77.0 / 255.0, blue: 59.0 / 255.0, alpha: 1.0)
+    public static var background = UIColor(red:0.99, green:0.47, blue:0.08, alpha:1.0)
     public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
     public static var title = UIColor.whiteColor()
     public static var subtitle = UIColor.whiteColor()
